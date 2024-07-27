cf33e46a1bbab0387b0cc7f238e897ec73781a2b
b166erobot <bernard0freitas@outlook.com>
2024-07-27 18:30:35 -0300

    Requisito 6 satisfeito

---
+++ src/06_challenge.sql
@@ -0,0 +1,8 @@
+UPDATE users SET active = TRUE
+WHERE full_name = 'Andressa';
+
+UPDATE users SET active = FALSE
+WHERE full_name = 'Rogério';
+
+UPDATE users SET plan_id = 2
+WHERE full_name = 'Camila';
